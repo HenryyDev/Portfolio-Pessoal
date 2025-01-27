bd99902e82c83a45ac0900eeaffe94c845deb1d7
henry4i20 <henrimito1305@gmail.com>
2025-01-27 20:14:50 -0300

    feat: adiciona a biblioteca TypewriterJS para efeito no h1 e fix de detalhes

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ toggleButton.addEventListener("click", () => {
     themeIcon.alt = "Light Mode";
   }
 });
+
+const typewriter = new Typewriter("#typewriter", {
+  loop: true, // Faz o texto repetir
+  delay: 75, // Velocidade de digitação
+});
+
+typewriter
+  .typeString("Olá, eu sou o <strong>Henry</strong>")
+  .pauseFor(1500) // Pausa antes de apagar
+  .deleteAll() // Apaga o texto
+
+  .start(); // Inicia o loop
